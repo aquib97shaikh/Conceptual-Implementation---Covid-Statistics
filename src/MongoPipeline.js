ebfbc3f66a64dff4de5714f0470865f412f3ac77
Aquib <aquibshaikh02397@gmail.com>
2020-12-10 15:00:56 +0530

    prettify

--- src/MongoPipeline.js
+++ src/MongoPipeline.js
@@ -1,72 +1,73 @@
 const pipeline = {
-    healthyStates :[
-        {
-          $project:{
-            _id:false,
-            state:"$state",
-            mortality:{
-              $round:[{
-                $divide:["$death","$infected"]
-              }
-                ,5
-              ]
-            }
-          }
+  healthyStates: [
+    {
+      $project: {
+        _id: false,
+        state: "$state",
+        mortality: {
+          $round: [
+            {
+              $divide: ["$death", "$infected"],
+            },
+            5,
+          ],
         },
-        {
-          $match:{
-            mortality:{$lt:0.005}
-          }
-        }
-      ],
-      hotspotStates:[
-        {
-          $project: {
-            _id: false,
-            state: "$state",
-            rate: {
-              $round: [
-                {
-                  $divide: [
-                    { $subtract: ["$infected", "$recovered"] },
-                    "$infected",
-                  ],
-                },
-                5,
+      },
+    },
+    {
+      $match: {
+        mortality: { $lt: 0.005 },
+      },
+    },
+  ],
+  hotspotStates: [
+    {
+      $project: {
+        _id: false,
+        state: "$state",
+        rate: {
+          $round: [
+            {
+              $divide: [
+                { $subtract: ["$infected", "$recovered"] },
+                "$infected",
               ],
             },
-          },
+            5,
+          ],
         },
-        {
-          $match: {
-            rate: { $gt: 0.1 },
-          },
-        },
-      ],
-      totalRecovered : [
-        {
-          $group: {
-            _id: "total",
-            recovered: { $sum: "$recovered" },
-          },
-        },
-      ],
-      totalActive : [
-        {
-          $group: {
-            _id: "total",
-            active: { $sum: { $subtract: ["$infected", "$recovered"] } },
-          },
-        },
-      ],
-      totalDeath :[
-        {
-          $group: {
-            _id: "total",
-            death: { $sum: "$death" },
-          },
-        },
-      ]
+      },
+    },
+    {
+      $match: {
+        rate: { $gt: 0.1 },
+      },
+    },
+  ],
+  totalRecovered: [
+    {
+      $group: {
+        _id: "total",
+        recovered: { $sum: "$recovered" },
+      },
+    },
+  ],
+  totalActive: [
+    {
+      $group: {
+        _id: "total",
+        active: { $sum: { $subtract: ["$infected", "$recovered"] } },
+      },
+    },
+  ],
+  totalDeath: [
+    {
+      $group: {
+        _id: "total",
+        death: { $sum: "$death" },
+      },
+    },
+  ],
 };
 
 exports.pipeline = pipeline;
