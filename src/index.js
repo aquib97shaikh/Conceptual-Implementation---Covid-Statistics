f0de0d7ddae3c06524a534a5a281a696492d9680
Aquib <aquibshaikh02397@gmail.com>
2020-12-10 13:27:10 +0530

    gte added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
-const { data } = require('./data')
+const port = 8080;
+const { data } = require("./data");
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connection } = require('./connector')
+const { connection } = require("./connector");
 
-app.get("/totalRecovered", async (req,res) =>{
+app.get("/totalRecovered", async (req, res) => {
   let result = await connection.aggregate([
     {
       $group: {
@@ -24,40 +24,60 @@ app.get("/totalRecovered", async (req,res) =>{
   res.send({
     data: result[0],
   });
-})
+});
 
-app.get("/totalActive",async (req,res) =>{
+app.get("/totalActive", async (req, res) => {
   let result = await connection.aggregate([
     {
-      $group:{
-        _id:"total",
-        active:{$sum:{$subtract:["$infected","$recovered"]}
-        }
-      }
-    }
+      $group: {
+        _id: "total",
+        active: { $sum: { $subtract: ["$infected", "$recovered"] } },
+      },
+    },
   ]);
   res.send({
-    data:result[0]
+    data: result[0],
   });
+});
 
-})
-
-app.get("/totalDeath", async (req,res)=>{
+app.get("/totalDeath", async (req, res) => {
   let result = await connection.aggregate([
     {
-      $group:{
-        _id:"total",
-        death:{$sum:"$death"}
-      }
-    }
+      $group: {
+        _id: "total",
+        death: { $sum: "$death" },
+      },
+    },
   ]);
   res.send({
-    data:result[0]
+    data: result[0],
   });
-} )
+});
 
-app.get("/hotspotStates",async (req,res) =>{
-  let result = await connection.aggregate([
+app.get("/hotspotStates", async (req, res) => {
+  // let result = await connection.aggregate([
+  //   {
+  //     $project: {
+  //       _id: false,
+  //       state: "$state",
+  //       rate: {
+  //         $round: [
+  //           {
+  //             $divide: [
+  //               { $subtract: ["$infected", "$recovered"] },
+  //               "$infected",
+  //             ],
+  //           },
+  //           5,
+  //         ],
+  //       },
+  //     },
+  //   },
+  //   {
+  //     $match: { $gte: 0.1 },
+  //   },
+  // ]);
+  let pipeline = [
     {
       $project: {
         _id: false,
@@ -67,7 +87,7 @@ app.get("/hotspotStates",async (req,res) =>{
             {
               $divide: [
                 { $subtract: ["$infected", "$recovered"] },
-                "$infected",
+                "$recovered",
               ],
             },
             5,
@@ -75,13 +95,18 @@ app.get("/hotspotStates",async (req,res) =>{
         },
       },
     },
-  ]);
+    {
+      $match: {
+        rate: { $gte: 0.1 },
+      },
+    },
+  ];
+  let result = await connection.aggregate(pipeline);
   res.send({
-    data:result,
-  })
-})
+    data: result,
+  });
+});
 
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
